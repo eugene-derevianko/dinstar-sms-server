9dd83e69d9e1009e0022e4abbbec8de2c5139251
Eugene Derevyanko <eugene.derevyanko@gmail.com>
2016-03-24 12:33:14 +0200

    try git worktrees feature

--- src/main/java/com/symulakr/dinstar/smsserver/Application.java
+++ src/main/java/com/symulakr/dinstar/smsserver/Application.java
@@ -2,6 +2,8 @@ package com.symulakr.dinstar.smsserver;
 
 import java.io.IOException;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -9,10 +11,13 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class Application
 {
+   private final static Logger LOG = LogManager.getLogger(SmsServer.class);
+
 
    public static void main(String[] args) throws IOException
    {
       ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+      LOG.info(context.getBeanDefinitionNames());
    }
 
 }
